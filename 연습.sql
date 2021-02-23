f1998a4adb667dc94c55b6dad33a2bc6d99d2497
kepchef <74166655+kepchef@users.noreply.github.com>
2021-02-23 18:00:50 +0900

    Delete 연습.sql

--- 연습.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `JBOARD_ARTICLE` (`title`,`content`,`uid`,`regip`,`rdate`)
-SELECT `title`,`content`,`uid`,`regip`,`rdate` FROM `JBOARD_ARTICLE`;
-
-SELECT * FROM `JBOARD_ARTICLE` ORDER BY `seq` DESC
-LIMIT 0, 10;
