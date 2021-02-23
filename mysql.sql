5805d34638df16c58d8d0812cc70e7e754b23ea7
kepchef <74166655+kepchef@users.noreply.github.com>
2021-02-23 18:00:32 +0900

    Delete mysql.sql

--- mysql.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'sqoop'@'localhost' identified by '1234';
-CREATE USER 'sqoop'@'%' identified by '1234';
-CREATE DATABASE sqoop;
-GRANT ALL PRIVILEGES ON sqoop.* TO 'sqoop'@'localhost';
-FLUSH PRIVILEGES;
